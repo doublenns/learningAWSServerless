4735617396add32e4ef189d75b3985b2dbeebf5b
DoubleNNs <nnamdianyanwu@gmail.com>
2021-01-06 23:48:33 -0600

    Add function to get old access keys

--- udemy-advanced_serverless_workflow_with_step_functions/get_expired_users.py
+++ udemy-advanced_serverless_workflow_with_step_functions/get_expired_users.py
@@ -1,7 +1,18 @@
 import boto3
+from datetime import datetime, timezone
+
+
+# boto3.setup_default_session(profile_name="default")
 
 iam_resource = boto3.resource("iam")
 iam_client = boto3.client("iam")
+
+
+def _time_diff(datekeycreated):
+    now = datetime.now(timezone.utc)
+    diff = now - datekeycreated
+    return diff.days
+
 
 def get_iam_accounts():
     iam_accounts = {}
@@ -10,9 +21,35 @@ def get_iam_accounts():
         iam_accounts[user_data["UserName"]] = user_data["Path"]
     return iam_accounts
 
+
+def get_expired_keys():
+    result = {}
+    expired_keys = []
+
+    for username, path in get_iam_accounts().items():
+        # if path == event["UserPath"]
+        if path == "/test_users/":
+            metadata = iam_client.list_access_keys(UserName=username)
+            if metadata["AccessKeyMetadata"]:
+                for key in metadata["AccessKeyMetadata"]:
+                    key_age = _time_diff(key["CreateDate"])
+                    if key["Status"] == "Active" and (
+                        # key_age > 30
+                        key_age < 30
+                    ):
+                        expired_keys.append(
+                            {
+                                "UserName": username,
+                                "AccessKey": key["AccessKeyId"],
+                                "KeyAge": key_age,
+                                "UserPath": path
+                            }
+                        )
+        result["Users"] = expired_keys
+        return result
+
 def main():
-    get_iam_accounts()
-    print(get_iam_accounts())
+    print(get_expired_keys())
 
 
 if __name__ == "__main__":
